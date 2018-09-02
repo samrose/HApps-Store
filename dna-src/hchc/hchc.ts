f9077aefa53395556bd334bd0072272ca96324c9
zo-el <joelulahanna@gmail.com>
2018-09-01 20:57:18 -0600

    created the files for the app with schemas

---
+++ dna-src/hchc/hchc.ts
@@ -0,0 +1,86 @@
+'use strict';
+export = 0;
+let module = {};
+// -----------------------------------------------------------------
+//  Public Functions
+// Author : Zo-El
+// -----------------------------------------------------------------
+// Description :
+// This zome can be used to manage the apps that are going to be added to the HC of HC
+// It includes :
+// - adding creating an aoo
+// - adding the code for that apps
+// - adding UI-Skins for apps
+// -----------------------------------------------------------------
+
+
+// -----------------------------------------------------------------
+//  The Genesis Function https://developer.holochain.org/genesis
+// -----------------------------------------------------------------
+
+function genesis() {
+  return true;
+}
+
+// -----------------------------------------------------------------
+//  Validation functions for every change to the local chain or DHT
+// -----------------------------------------------------------------
+
+function validateCommit(entryName, entry, header, pkg, sources) {
+  // debug("entryName: " + entryName + " entry: " + entry + " header: " + header + " pkg: " + pkg + " sources: " + sources)
+  switch (entryName) {
+    case "ratings":
+      return true;
+    case "ratingsLink":
+      return true;
+    default:
+      return false;
+  }
+}
+
+function validatePut(entryName, entry, header, pkg, sources) {
+  // debug("entryName: " + entryName + " entry: " + entry + " header: " + header + " pkg: " + pkg + " sources: " + sources)
+  switch (entryName) {
+    case "ratings":
+      return true;
+    case "ratingsLink":
+      return true;
+    default:
+      return false;
+  }
+}
+
+function validateMod(entryName, entry, header, replaces, pkg, sources) {
+  switch (entryName) {
+    default:
+      return false;
+  }
+}
+
+function validateDel(entryName, hash, pkg, sources) {
+  switch (entryName) {
+    default:
+      return false;
+  }
+}
+function validateLink(entryName, baseHash, links, pkg, sources) {
+  // debug("entryName: " + entryName + " baseHash: " + baseHash + " links: " + links + " pkg: " + pkg + " sources: " + sources)
+  switch (entryName) {
+    case "ratingsLink":
+      return true;
+    default:
+      return false;
+  }
+}
+function validatePutPkg(entryName) {
+  return null;
+}
+function validateModPkg(entryName) {
+  return null;
+}
+function validateDelPkg(entryName) {
+  return null;
+}
+function validateLinkPkg(entryName) {
+  return null;
+}
