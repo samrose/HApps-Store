08d647c35ab84d9e612e14b941f29b509e8e88ff
Willem Olding <willemolding@gmail.com>
2019-01-28 16:04:28 +0100

    adds holochain actions

--- ui-src/src/actions.ts
+++ ui-src/src/actions.ts
@@ -14,6 +14,14 @@ interface App {
     thumbnail: string
 }
 
+interface DnaBundle {
+    dna_bundle: string,
+}
+
+interface UiBundle {
+    ui_bundle: string,
+}
+
 interface GetLinksLoadElement<T> {
   address: string,
   entry: T
@@ -44,3 +52,62 @@ export const Whoami = createHolochainAsyncAction<
 {hash: string, name: string}
 >
 (`happ-store`, 'whoami', 'main', 'get_user')
+
+export const AddDna = createHolochainAsyncAction<
+{app_hash: string, dna_bundle: string}, 
+string
+>
+(`happ-store`, 'happs', 'main', 'add_dna')
+
+export const GetDna = createHolochainAsyncAction<
+{app_hash: string}, 
+DnaBundle
+>
+(`happ-store`, 'happs', 'main', 'get_dna')
+
+export const AddUi = createHolochainAsyncAction<
+{app_hash: string, dna_bundle: string}, 
+string
+>
+(`happ-store`, 'happs', 'main', 'add_ui')
+
+export const GetUi = createHolochainAsyncAction<
+{app_hash: string}, 
+UiBundle
+>
+(`happ-store`, 'happs', 'main', 'get_ui')
+
+export const AddAppToCategory = createHolochainAsyncAction<
+{app_address: string, category: string}, 
+{}
+>
+(`happ-store`, 'happs', 'main', 'add_app_to_category')
+
+export const AddAppToTag = createHolochainAsyncAction<
+{app_address: string, category: string}, 
+{}
+>
+(`happ-store`, 'happs', 'main', 'add_app_to_tag')
+
+export const GetAppsByCategory = createHolochainAsyncAction<
+{app_address: string, category: string}, 
+{}
+>
+(`happ-store`, 'happs', 'main', 'get_apps_by_category')
+
+export const GetAppsByTag = createHolochainAsyncAction<
+{app_address: string, category: string}, 
+{}
+>
+(`happ-store`, 'happs', 'main', 'get_apps_by_tag')
+
+// create_ratings: {
+//     inputs:| rate:String, review:String, reviewed_hash: Address |,
+//     outputs: | result: ZomeApiResult<Address> |,
+//     handler: ratings::handlers::handle_creating_ratings
+// }
+// get_ratings: {
+//     inputs:| reviewed_hash: Address |,
+//     outputs: |result: ZomeApiResult<Vec<GetLinksLoadElement<Ratings>>>|,
+//     handler: ratings::handlers::handle_get_reviews_by_hash
+// }
