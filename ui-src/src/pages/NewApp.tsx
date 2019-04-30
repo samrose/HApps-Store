ce87fd1ac52684d5b96d984ffa6d4cded5bb3a8b
Lisa <ldjetton21@gmail.com>
2019-04-30 00:13:45 -0500

    updated app form to take in multiple dnas, and added hashes for ui and dna sources in app bundle

--- ui-src/src/pages/NewApp.tsx
+++ ui-src/src/pages/NewApp.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import * as classnames from "classnames";
 import { render } from "react-dom";
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import AppCard from '../components/AppCard';
+import FormControl from '@material-ui/core/FormControl';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
@@ -11,23 +14,38 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import AppCard from '../components/AppCard'
+import AddIcon from '@material-ui/icons/AddCircleOutline';
+import RemoveIcon from '@material-ui/icons/RemoveCircleOutline';
 
-import { AppCreationSpec, defaultAppCreationSpec } from '../types/app'
+import { CreateApp } from '../actions';
+import { AppCreationSpec, defaultAppCreationSpec, AppResource } from '../types/app'
 import snakecase from 'snakecase-keys'
 
 const styles = (theme: Theme) =>
   createStyles({
+      containerRoot: {
+        marginTop:theme.spacing.unit * 5,
+      },
       root: {
       ...theme.mixins.gutters(),
-      paddingTop: theme.spacing.unit * 2,
-      paddingBottom: theme.spacing.unit * 2,
-      color: "#fff",
-      backgroundColor: "#016e76f7"
+      paddingTop: theme.spacing.unit * 4,
+      paddingBottom: theme.spacing.unit * 4,
+      backgroundColor: "#dededf",
+      border: '10px solid #016e76f7'
     },
     container: {
       display: 'flex',
       flexWrap: 'wrap',
+    },
+    exampleCard: {
+      marginLeft: 25 // -30
+    },
+    margin: {
+      minWidth: '42vw',
+      margin: '0 auto'
+    },
+    heading: {
+      textAlign: 'center'
     },
     textField: {
       marginLeft: theme.spacing.unit,
@@ -43,6 +61,20 @@ const styles = (theme: Theme) =>
     progress: {
       margin: theme.spacing.unit * 2,
     },
+    btn: {
+      marginTop: 45,
+      marginBottom: 5
+    },
+    leftColumn: {
+      marginRight: 10,
+    },
+    rightColumn: {
+      marginLeft: 10,
+    },
+    progressDialog:{
+      minWidth: 150,
+      minHeight: 150,
+    }
   });
 
 
@@ -54,153 +86,256 @@ export interface Props extends WithStyles<typeof styles> {
 export interface State {
   previewOpen: boolean,
   appInput: AppCreationSpec,
+  uiUrl: string,
+  uiHash: string,
+  dnaUrl: string,
+  dnas: Array<AppResource>,
+  dnaAmount: Array<string>
 }
 
+export type StateKeys = string | number | symbol | any;
 
 class NewApp extends React.Component<Props, State> {
-
   public state: State = {
     previewOpen: false,
-    appInput: defaultAppCreationSpec
+    appInput: defaultAppCreationSpec,
+    uiUrl: '',
+    uiHash: '',
+    dnaUrl: '',
+    dnas: [{
+      location: '',
+      hash: '',
+    }],
+    dnaAmount: ['']
   }
 
   public render() {
-
-    const { classes } = this.props
-
+    const { classes } = this.props;
     return (
-      <div>
+      <Grid className={classes.containerRoot}
+        container={true}
+        spacing={16}
+        direction="row"
+        alignItems="center"
+        justify="center" // space-evenly
+      >
       <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
-          LALAL Provide the following details to post a new hApp to the store
-        </Typography>
-        <Typography component="p">
-          Keep in mind that anything submitted will be visible to all participants for the lifetime of the hApp store.<br/>
-          Please be considerate, check submissions carefully and keep cruft to a minimum.
-        </Typography>
-         <Grid container={true} spacing={24}>
-           <Grid item={true} xs={12} sm={6}>
-            <TextField
-              id="title-field"
-              required={true}
-              label="Title"
-              value={this.state.appInput.title}
-              onChange={this.handleChange('title')}
-              className={classes.textField}
-
-            />
-           </Grid>
-
         <Grid item={true} xs={12}>
-          <TextField
-            id="description-field"
-            label="Description"
-            multiline={true}
-            fullWidth={true}
-            value={this.state.appInput.description}
-            onChange={this.handleChange('description')}
-            className={classes.textField}
-          />
-           </Grid>
-
-        <Grid item={true} xs={12} sm={6}>
-          <TextField
-            id="thumburl-field"
-            label="Thumbnail URL"
-            value={this.state.appInput.thumbnailUrl}
-            onChange={this.handleChange('thumbnailUrl')}
-            className={classes.textField}
-          />
-           </Grid>
-
-        <Grid item={true} xs={12} sm={6}>
-          <TextField
-            id="homepage-field"
-            required={true}
-            label="hApp Homepage URL"
-            value={this.state.appInput.homepageUrl}
-            onChange={this.handleChange('homepageUrl')}
-            className={classes.textField}
-          />
-           </Grid>
-
-        { /* TODO: update for multiple DNAs and ability to supply or detect hash */}
-        <Grid item={true} xs={12} sm={6}>
-          <TextField
-            id="dna-field"
-            required={true}
-            label="DNA URL (dna.json file)"
-            value={(this.state.appInput.dnas[0] || {location: ''} as any).location}
-            onChange={this.handleChange('dnaUrl')}
-            className={classes.textField}
-          />
+          <Typography variant="h4" component="h3" className={classes.heading}>
+            Provide the following details to post a new hApp to the store
+          </Typography>
+          <Typography component="caption" className={classes.heading}>
+            Keep in mind that anything submitted will be visible to all participants for the lifetime of the hApp store.<br/>
+            Please be considerate, check submissions carefully and keep cruft to a minimum.
+          </Typography>
         </Grid>
 
-        { /* TODO: update for multiple DNAs and ability to supply or detect hash */}
-        <Grid item={true} xs={12} sm={6}>
-          <TextField
-            id="dna-field"
-            required={true}
-            label="DNA URL (dna.json file)"
-            value={(this.state.appInput.dnas[0] || {location: ''} as any).location}
-            onChange={this.handleChange('dnaUrl')}
-            className={classes.textField}
-          />
+        <Grid
+          container={true}
+          spacing={24}
+          direction="row"
+          alignItems="center"
+          justify="center"
+        >
+          <form>
+            <Grid container={true} direction="column" justify="center" alignItems="center">
+                <Grid item={true} xs={12}>
+                  <TextField
+                    id="title-field"
+                    required={true}
+                    label="Title"
+                    value={this.state.appInput.title}
+                    onChange={this.handleChange('title')}
+                    className={classes.textField}
+                  />
+                </Grid>
+
+                <Grid item={true} xs={12}>
+                    <TextField
+                      id="description-field"
+                      label="Description"
+                      multiline={true}
+                      fullWidth={true}
+                      value={this.state.appInput.description}
+                      onChange={this.handleChange('description')}
+                      className={classes.textField}
+                    />
+                </Grid>
+                <Grid item={true} xs={12}>
+                  <TextField
+                    id="thumburl-field"
+                    label="Thumbnail URL"
+                    value={this.state.appInput.thumbnailUrl}
+                    onChange={this.handleChange('thumbnailUrl')}
+                    className={classes.textField}
+                  />
+                </Grid>
+                <Grid item={true} xs={12}>
+                  <TextField
+                    id="homepage-field"
+                    required={true}
+                    label="hApp Homepage URL"
+                    value={this.state.appInput.homepageUrl}
+                    onChange={this.handleChange('homepageUrl')}
+                    className={classes.textField}
+                  />
+                </Grid>
+
+                <Grid item={true} xs={12}>
+                  <TextField
+                    id="ui-field"
+                    label="UI URL (zip file)"
+                    value={(this.state.appInput.ui! || {location: ''} as any).location}
+                    onChange={this.handleSourceCodeChange('uiUrl')}
+                    className={classes.textField}
+                  />
+                </Grid>
+                <Grid item={true} xs={12}>
+                  <TextField
+                    id="ui-field"
+                    label="UI Hash"
+                    value={(this.state.appInput.ui! || {hash: ''} as any).hash}
+                    onChange={this.handleSourceCodeChange('uiHash')}
+                    className={classes.textField}
+                  />
+                </Grid>
+
+                <br/>
+                <br/>
+                <Typography component="caption" className={classes.heading}>
+                  Please add the Hash and URL for each DNA service included in your hApp Bundle.
+                </Typography>
+                <div>
+                  <span onClick={this.addDnaLine} style={{display:'inline-flex'}}>
+                    <AddIcon style={{color:'#0e094b'}}/>
+                  </span>
+                  <span onClick={this.removeDnaLine} style={{display:'inline-flex'}}>
+                    <RemoveIcon style={{color:'#0e094b'}}/>
+                  </span>
+                </div>
+
+                <Grid container={true} direction="row" justify="flex-end">
+                  <div className={classes.rightColumn}>
+                  <Grid container={true} direction="column" justify="flex-end">
+                    <Grid item={true} xs={6}>
+                      <FormControl>
+                         {this.state.dnaAmount.map((value, i) => (
+                           <TextField
+                             key={i}
+                             id="dna-field"
+                             required={true}
+                             label="DNA URL (dna.json file)"
+                             value={(this.state.dnas[i]! || {location: ''} as any).location}
+                             onChange={this.handleDnaChange(i, "location")}
+                             className={classes.textField}
+                           />
+                         ))}
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+              </div>
+
+              <div className={classes.rightColumn}>
+                <Grid container={true} direction="column" justify="flex-end">
+                  <Grid item={true} xs={6}>
+                    <FormControl>
+                       {this.state.dnaAmount.map((value, i) => (
+                         <TextField
+                           key={i}
+                           id="dna-field"
+                           required={true}
+                           label="DNA Hash"
+                           value={(this.state.dnas[i]! || {hash: ''} as any).hash}
+                           onChange={this.handleDnaChange(i,"hash")}
+                           className={classes.textField}
+                         />
+                       ))}
+                    </FormControl>
+                  </Grid>
+
+                </Grid>
+              </div>
+            </Grid>
+          </Grid>
+        </form>
+
+        <br/>
+        <Grid container={true} direction="column" alignItems="center" justify="center" className={classes.btn}>
+          <Grid item={true} xs={12}>
+            <Button variant="contained" size="large" color="primary" onClick={this.handleClickSubmit(this.state.appInput)}>
+                Submit
+            </Button>
+          </Grid>
         </Grid>
 
-        <Grid item={true} xs={12} sm={6}>
-          <TextField
-            id="ui-field"
-            label="UI URL (zip file)"
-            value={(this.state.appInput.ui || {location: ''} as any).location}
-            onChange={this.handleChange('uiUrl')}
-            className={classes.textField}
-          />
         </Grid>
+      </Paper>
 
-        <Grid item={true} xs={6} sm={6}>
-          <AppCard app={ {
-            author: "<your holochain ID will go here>",
+      <Paper className={classnames(classes.root, classes.exampleCard)}>
+        <AppCard app={ {
+          author: "<your holochain ID will go here>",
             address: "",
             upvotes: 0,
             upvotedByMe: false,
             appEntry: this.state.appInput
-          } }/>
-        </Grid>
+        }}/>
+      </Paper>
 
-        </Grid>
+      <Dialog open={this.props.awaitingResponse} className={classes.progressDialog}>
+        <CircularProgress className={classes.progress}/>
+      </Dialog>
 
-          <Button variant="contained" size="large" color="primary" onClick={this.handleClickSubmit(this.state.appInput)}>
-            Submit
-          </Button>
+    </Grid>
+  );}
 
-        </Paper>
-
-        <Dialog open={this.props.awaitingResponse}>
-          <CircularProgress className={classes.progress}/>
-        </Dialog>
-
-
-        </div>
-    );
+  private addDnaLine = () => {
+    // hack to add add'l lines... refactor
+    const newAmount:Array<string> = this.state.dnaAmount;
+    newAmount.push('i');
+    this.setState({
+      dnaAmount: newAmount
+    });
+  }
+  private removeDnaLine = () => {
+    // hack to add add'l lines... refactor
+    if(this.state.dnaAmount.length >= 1) {
+      const newAmount:Array<string> = this.state.dnaAmount;
+      newAmount.pop();
+      this.setState({
+        dnaAmount: newAmount
+      });
+    }
   }
 
-  private handleChange = (name: keyof AppCreationSpec | 'dnaUrl' | 'uiUrl') => (event: React.ChangeEvent<HTMLInputElement>) => {
-    // UPDATE THE DNA to accept hash as well...
-    let update
-    const value = event.target.value
-    if (name === 'dnaUrl') {
-      update = { ...this.state.appInput, dnas: [{ location: value, hash: 'TODO' }] }
-    } else if (name === 'uiUrl') {
-      update = { ...this.state.appInput, ui: { location: value, hash: 'TODO' } }
-    } else {
-      update = { ...this.state.appInput, [name]: value }
-    }
+  private handleDnaChange = (dnaNum:number, type:string ) => (event: any) => {
+    console.log("handleDnaChange event", event);
+    const newDnaList = this.state.dnas;
+    dnaNum++;
+    newDnaList[dnaNum][type] = event.target.value;
+
+    const newDNAvalue = newDnaList[dnaNum][type] = event.target.value;
+    console.log("newDNAvalue", newDNAvalue);
+    this.setState({ dnas: newDnaList });
+  };
+
+  private handleSourceCodeChange = (title:StateKeys) => (event: any) => {
+    console.log("handleSourceCodeChange event", event);
+    console.log("handleSourceCodeChange title", title);
+    this.setState({ [title]: event.target.value } as Pick<State, keyof State>);
+  };
+  private handleChange = (name: keyof AppCreationSpec) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const update = { ...this.state.appInput, [name]: value };
     this.setState({ appInput: update as AppCreationSpec});
   }
 
-  private handleClickSubmit = (app: AppCreationSpec) => (event: React.MouseEvent<HTMLElement>) => {
-    this.props.createApp(app).then(() => {
+  private handleClickSubmit = (app: AppCreationSpec) => (event: any) => {
+    console.log("fn app param", app);
+
+    const appWithFiles = { ...app, dnas: this.state.dnas, ui: { location: this.state.uiUrl, hash: this.state.uiHash } }
+    console.log("appWithFiles", appWithFiles);
+
+    this.props.createApp(appWithFiles).then(() => {
       // @ts-ignore
       this.props.history.push('/')
     })
@@ -210,12 +345,20 @@ class NewApp extends React.Component<Props, State> {
     this.setState({previewOpen: open})
   }
 
+  private clearValues = () => {
+    this.setState({
+      uiUrl: '',
+      uiHash: '',
+      dnas: [{
+        location: '',
+        hash: '',
+      }],
+      dnaAmount: ['']
+    })
+  }
 }
 
-import { CreateApp } from '../actions'
-
 const mapStateToProps = ({ awaitingResponse }) => ({ awaitingResponse });
-
 const mapDispatchToProps = dispatch => ({
   createApp: (app: AppCreationSpec) => dispatch(CreateApp.create(snakecase(app))),
 });
