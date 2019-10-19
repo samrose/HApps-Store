b434f5a6f2368fa21a616dfeff0d673ab17283c0
zo-el <joelulahanna@gmail.com>
2019-10-18 19:04:15 -0500

    updated to v 0.0.32-alpha2

--- hApp-store/test/single_agent_tests/whoami_test.js
+++ hApp-store/test/single_agent_tests/whoami_test.js
@@ -1,8 +1,14 @@
+const { one } = require('../config')
 
 module.exports = (scenario) => {
-  scenario('get user address', async (s, t, {liza}) => {
-    const result = await liza.call("whoami", "get_user", {})
+  scenario('get user address', async (s, t) => {
+
+    const { liza } = await s.players({liza: one('liza')}, true)
+
+    const result = await liza.call( "app", "whoami", "get_user", {})
     console.log(result)
-    t.equal(result.Ok.hash.length, 63) // agent addresses are 63 chars long
+    t.ok(result.Ok)
+
+    await liza.kill()
   })
 }
