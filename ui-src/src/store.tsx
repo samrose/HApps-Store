96bc8ce97a4d4e00bfc36d40e40d6417e6ceaaa9
zo-el <joelulahanna@gmail.com>
2019-10-29 13:51:30 -0500

    updated the UI to work with the new hc-web-client

--- ui-src/src/store.tsx
+++ ui-src/src/store.tsx
@@ -7,9 +7,9 @@ import reducer from './reducer'
 import { URL } from './utils'
 
 // in production the conductor will tell us where to connect. For dev must speficy a port
-const url = process.env.NODE_ENV === 'production' ? undefined : 'ws://localhost:4000'
+const url = process.env.NODE_ENV === 'production' ? undefined : 'ws://localhost:10002'
 // put middleware in this array to have it applied
-const middleware: Array<any> = [holochainMiddleware(connect(url))]
+const middleware: Array<any> = [holochainMiddleware(connect({url}))]
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
